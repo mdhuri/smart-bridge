4a211d5e0c98a7673c4adc60358f68afbfd992f1
madhuri <51332677+mdhuri@users.noreply.github.com>
2019-06-12 14:33:34 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,23 +1,18 @@
 /*
- * relay.c
+ * switch lcd.c
  *
- * Created: 11-06-2019 15:48:58
+ * Created: 08-06-2019 14:42:20
  * Author : Madhu
  */ 
 
 #include <avr/io.h>
-#include <util/delay.h>
-#include <stdlib.h>
+
+
 int main(void)
 {
-	DDRD=0XFF;
-	DDRB=0X00;
-	while (1)
-	{
-		if((PINB&(1<<PB0))==0)
-		PORTD=0X01;
-		_delay_ms(500);
-	
-    
-	}
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
 }
+
